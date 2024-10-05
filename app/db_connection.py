e4801f95c697ef3f753e81044c79eb5cb739ed4d
Ajay Dev <devadigaajay1729@gmail.com>
2024-10-05 23:49:42 +0530

    databse url issue

--- app/db_connection.py
+++ app/db_connection.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DEV_DATABASE_URL = os.getenv('DEV_DATABASE_URL')
 
-engine = create_engine('DEV_DATABASE_URL')
+engine = create_engine(DEV_DATABASE_URL)
 local_session = sessionmaker(autocommit=False, autoflush=True, bind=True)
 Base = declarative_base()
 
