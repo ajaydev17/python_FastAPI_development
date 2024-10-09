402bc58f29a837bd8ebaabf41863f14cddafdee3
Ajay Dev <devadigaajay1729@gmail.com>
2024-10-09 23:33:35 +0530

    Added pytest for category table.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
 from .db_connection import Base
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String, Boolean
 
 
 class Category(Base):
     __tablename__ = 'category'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    slug = Column(String(120), nullable=False)
+    is_active = Column(Boolean, nullable=False)
+    level = Column(Integer, nullable=False)
+    parent_id = Column(Integer, nullable=True)
--- tests/models/test_category.py
+++ tests/models/test_category.py
@@ -2,6 +2,7 @@
 ## Table and Column Validation
 """
 from .fixtures import db_inspector
+from sqlalchemy import Integer, String, Boolean
 
 """
 - [ ] Confirm the presence of all required tables within the database schema.
@@ -16,9 +17,41 @@ def test_model_structure_table_exists(db_inspector):
 - [ ] Validate the existence of expected columns in each table, ensuring correct data types.
 """
 
+
+def test_model_structure_column_data_type(db_inspector):
+    table = 'category'
+    columns = {columns['name']: columns for columns in db_inspector.get_columns(table)}
+
+    assert isinstance(columns['id']['type'], Integer)
+    assert isinstance(columns['name']['type'], String)
+    assert isinstance(columns['slug']['type'], String)
+    assert isinstance(columns['is_active']['type'], Boolean)
+    assert isinstance(columns['level']['type'], Integer)
+    assert isinstance(columns['parent_id']['type'], Integer)
+
+
 """
 - [ ] Verify nullable or not nullable fields
 """
+
+
+def test_model_structure_nullable_constraints(db_inspector):
+    table = 'category'
+    columns = db_inspector.get_columns(table)
+
+    expected_nulls = {
+        'id': False,
+        'name': False,
+        'slug': False,
+        'is_active': False,
+        'level': False,
+        'parent_id': True
+    }
+
+    for column in columns:
+        column_name = column['name']
+        assert column['nullable'] == expected_nulls.get(column_name), f"column '{column_name}' is not nullable as expected."
+
 
 """
 - [ ] Test columns with specific constraints to ensure they are accurately defined.
