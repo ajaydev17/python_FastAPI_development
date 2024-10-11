d1833e6b569bae141cf9ddf1dca201a162a10088
Ajay Dev <devadigaajay1729@gmail.com>
2024-10-11 23:53:45 +0530

    Added product table

--- app/models.py
+++ app/models.py
@@ -1,20 +1,64 @@
 from .db_connection import Base
-from sqlalchemy import Column, Integer, String, Boolean, CheckConstraint, UniqueConstraint
+from sqlalchemy import Column, Integer, String, Boolean, CheckConstraint, UniqueConstraint, text, Text, DateTime, Enum, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+import sqlalchemy
 
 
 class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(100), nullable=False)
-    slug = Column(String(120), nullable=False)
+    name = Column(String(100), nullable=False, unique=True)
+    slug = Column(String(120), nullable=False, unique=True)
     is_active = Column(Boolean, nullable=False, default=False, server_default='False')
     level = Column(Integer, nullable=False, default='100', server_default='100')
-    parent_id = Column(Integer, nullable=True)
+    parent_id = Column(Integer, ForeignKey("category.id"), nullable=True)
 
     __table_args__ = (
-        CheckConstraint('LENGTH(name) > 0', name='name_length_check'),
-        CheckConstraint('LENGTH(slug) > 0', name='slug_length_check'),
+        CheckConstraint('LENGTH(name) > 0', name='category_name_length_check'),
+        CheckConstraint('LENGTH(slug) > 0', name='category_slug_length_check'),
         UniqueConstraint('name', 'level', name='uq_category_name_level'),
         UniqueConstraint('slug', name='uq_category_slug')
+    )
+
+
+class Product(Base):
+    __tablename__ = 'product'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    pid = Column(
+        UUID(as_uuid=True),
+        nullable=False,
+        server_default=text('uuid_generate_v4()')
+    )
+    name = Column(String(200), nullable=False)
+    slug = Column(String(220), nullable=False)
+    description = Column(Text, nullable=True)
+    is_digital = Column(Boolean, nullable=False, default=False, server_default="False")
+    created_at = Column(
+        DateTime,
+        server_default=text('CURRENT_TIMESTAMP'),
+        nullable=False
+    )
+    updated_at = Column(
+        DateTime,
+        server_default=text("CURRENT_TIMESTAMP"),
+        onupdate=sqlalchemy.func.now(),
+        nullable=False,
+    )
+    is_active = Column(Boolean, nullable=False, default=False, server_default="False")
+    stock_status = Column(
+        Enum("oos", "is", "obo", name="status_enum"),
+        nullable=False,
+        server_default="oos",
+    )
+    category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
+    seasonal_id = Column(Integer, ForeignKey("seasonal_event.id"), nullable=True)
+
+    __table_args__ = (
+        CheckConstraint("LENGTH(name) > 0", name="product_name_length_check"),
+        CheckConstraint("LENGTH(slug) > 0", name="product_slug_length_check"),
+        UniqueConstraint("name", name="uq_product_name"),
+        UniqueConstraint("slug", name="uq_product_slug"),
+        UniqueConstraint("pid", name="uq_product_pid"),
     )
--- tests/models/test_product.py
+++ tests/models/test_product.py
@@ -2,7 +2,8 @@
 ## Table and Column Validation
 """
 from .fixtures import db_inspector
-from sqlalchemy import Integer, String, Boolean
+from sqlalchemy import Integer, String, Boolean, Text, DateTime, Enum
+from sqlalchemy.dialects.postgresql import UUID
 
 """
 - [ ] Confirm the presence of all required tables within the database schema.
@@ -10,7 +11,7 @@ from sqlalchemy import Integer, String, Boolean
 
 
 def test_model_structure_table_exists(db_inspector):
-    assert db_inspector.has_table('category')
+    assert db_inspector.has_table('product')
 
 
 """
@@ -19,15 +20,21 @@ def test_model_structure_table_exists(db_inspector):
 
 
 def test_model_structure_column_data_type(db_inspector):
-    table = 'category'
+    table = 'product'
     columns = {columns['name']: columns for columns in db_inspector.get_columns(table)}
 
     assert isinstance(columns['id']['type'], Integer)
+    assert isinstance(columns['pid']['type'], UUID)
     assert isinstance(columns['name']['type'], String)
     assert isinstance(columns['slug']['type'], String)
+    assert isinstance(columns['description']['type'], Text)
+    assert isinstance(columns['is_digital']['type'], Boolean)
+    assert isinstance(columns['created_at']['type'], DateTime)
+    assert isinstance(columns['updated_at']['type'], DateTime)
     assert isinstance(columns['is_active']['type'], Boolean)
-    assert isinstance(columns['level']['type'], Integer)
-    assert isinstance(columns['parent_id']['type'], Integer)
+    assert isinstance(columns['stock_status']['type'], Enum)
+    assert isinstance(columns['category_id']['type'], Integer)
+    assert isinstance(columns['seasonal_event']['type'], Integer)
 
 
 """
@@ -36,16 +43,22 @@ def test_model_structure_column_data_type(db_inspector):
 
 
 def test_model_structure_nullable_constraints(db_inspector):
-    table = 'category'
+    table = 'product'
     columns = db_inspector.get_columns(table)
 
     expected_nulls = {
         'id': False,
+        'pid': False,
         'name': False,
         'slug': False,
+        'description': True,
+        'is_digital': False,
+        'created_at': False,
+        'updated_at': False,
         'is_active': False,
-        'level': False,
-        'parent_id': True
+        'stock_status': False,
+        'category_id': False,
+        'seasonal_event': True
     }
 
     for column in columns:
@@ -60,11 +73,11 @@ def test_model_structure_nullable_constraints(db_inspector):
 
 
 def test_model_structure_column_constraints(db_inspector):
-    table = 'category'
+    table = 'product'
     constraints = db_inspector.get_check_constraints(table)
 
-    assert any(constraint['name'] == 'category_name_length_check' for constraint in constraints)
-    assert any(constraint['name'] == 'category_slug_length_check' for constraint in constraints)
+    assert any(constraint['name'] == 'product_name_length_check' for constraint in constraints)
+    assert any(constraint['name'] == 'product_slug_length_check' for constraint in constraints)
 
 
 """
@@ -73,11 +86,12 @@ def test_model_structure_column_constraints(db_inspector):
 
 
 def test_model_structure_default_values(db_inspector):
-    table = 'category'
+    table = 'product'
     columns = {columns['name']: columns for columns in db_inspector.get_columns(table)}
 
+    assert columns['is_digital']['default'] == 'false'
     assert columns['is_active']['default'] == 'false'
-    assert columns['level']['default'] == '100'
+    assert columns['stock_status']['default'] == "'oos'::status_enum"
 
 
 """
@@ -86,11 +100,11 @@ def test_model_structure_default_values(db_inspector):
 
 
 def test_model_structure_column_lengths(db_inspector):
-    table = 'category'
+    table = 'product'
     columns = {columns['name']: columns for columns in db_inspector.get_columns(table)}
 
-    assert columns['name']['type'].length == 100
-    assert columns['slug']['type'].length == 120
+    assert columns['name']['type'].length == 200
+    assert columns['slug']['type'].length == 220
 
 
 """
@@ -99,7 +113,7 @@ def test_model_structure_column_lengths(db_inspector):
 
 
 def test_model_structure_unique_constraints(db_inspector):
-    table = 'category'
+    table = 'product'
     constraints = db_inspector.get_unique_constraints(table)
 
     assert any(constraint['name'] == 'uq_category_name_level' for constraint in constraints)
